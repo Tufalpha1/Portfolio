c514a741a64332d3ef04d4c752b0f9759a239448
unknown <zaynzafar18@gmail.com>
2023-10-28 21:03:31 +0500

    center heading

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -14,7 +14,7 @@ const Education = () => {
               Education
             </p>
           </div>
-          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <h2 className="max-w-lg mb-6 font-sans text-3xl text-center font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
             <span className="relative inline-block">
               <svg
                 viewBox="0 0 52 24"
