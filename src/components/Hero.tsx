c919f691022092521069fdddce8e5b79fe72f6d0
unknown <zaynzafar18@gmail.com>
2023-11-03 20:28:43 +0500

    revert back to old design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,7 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import Link from "next/link";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
+
 export const Hero = () => {
   const [text] = useTypewriter({
     words: ["Developer", "Designer"],
@@ -16,7 +17,10 @@ export const Hero = () => {
 
   let id = useId();
   return (
-    <section id="home" className="w-full h-screen text-center">
+    <section
+      id="home"
+      className="w-full h-screen text-center"
+    >
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -31,70 +35,7 @@ export const Hero = () => {
           />
         </div>
       </div>
-      <div className="hidden xl:block relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
-        <div className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-1 xl:ml-0">
-          <svg
-            viewBox="0 0 1026 1026"
-            fill="none"
-            aria-hidden="true"
-            className="absolute inset-0 h-full w-full animate-spin-slow"
-          >
-            <path
-              d="M1025 513c0 282.77-229.23 512-512 512S1 795.77 1 513 230.23 1 513 1s512 229.23 512 512Z"
-              stroke="#D4D4D4"
-              strokeOpacity="0.7"
-            />
-            <path
-              d="M513 1025C230.23 1025 1 795.77 1 513"
-              stroke={`url(#${id}-gradient-1)`}
-              strokeLinecap="round"
-            />
-            <defs>
-              <linearGradient
-                id={`${id}-gradient-1`}
-                x1="1"
-                y1="513"
-                x2="1"
-                y2="1025"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#1190CB" />
-                <stop offset="1" stopColor="#1190CB" stopOpacity="0" />
-              </linearGradient>
-            </defs>
-          </svg>
-          <svg
-            viewBox="0 0 1026 1026"
-            fill="none"
-            aria-hidden="true"
-            className="absolute inset-0 h-full w-full animate-spin-slower"
-          >
-            <path
-              d="M913 513c0 220.914-179.086 400-400 400S113 733.914 113 513s179.086-400 400-400 400 179.086 400 400Z"
-              stroke="#D4D4D4"
-              strokeOpacity="0.7"
-            />
-            <path
-              d="M913 513c0 220.914-179.086 400-400 400"
-              stroke={`url(#${id}-gradient-2)`}
-              strokeLinecap="round"
-            />
-            <defs>
-              <linearGradient
-                id={`${id}-gradient-2`}
-                x1="913"
-                y1="513"
-                x2="913"
-                y2="913"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#43cea2" />
-                <stop offset="1" stopColor="#43cea2" stopOpacity="0" />
-              </linearGradient>
-            </defs>
-          </svg>
-        </div>
-      </div>
+
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-700">
