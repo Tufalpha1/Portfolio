12b5dfcf7e091d284bf07824e5f49a1a88e4f9f5
unknown <zaynzafar18@gmail.com>
2023-11-03 19:28:51 +0500

    add background animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,7 +31,7 @@ export const Hero = () => {
           />
         </div>
       </div>
-      <div className="absolute left-1/2 top-56 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0">
+      <div className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-1 xl:ml-0">
         <svg
           viewBox="0 0 1026 1026"
           fill="none"
@@ -41,7 +41,7 @@ export const Hero = () => {
           <path
             d="M1025 513c0 282.77-229.23 512-512 512S1 795.77 1 513 230.23 1 513 1s512 229.23 512 512Z"
             stroke="#D4D4D4"
-            strokeOpacity="1"
+            strokeOpacity="0.7"
           />
           <path
             d="M513 1025C230.23 1025 1 795.77 1 513"
@@ -58,7 +58,7 @@ export const Hero = () => {
               gradientUnits="userSpaceOnUse"
             >
               <stop stopColor="#1190CB" />
-              <stop offset="1" stopColor="#1190CB" stopOpacity="50" />
+              <stop offset="1" stopColor="#1190CB" stopOpacity="0" />
             </linearGradient>
           </defs>
         </svg>
@@ -71,7 +71,7 @@ export const Hero = () => {
           <path
             d="M913 513c0 220.914-179.086 400-400 400S113 733.914 113 513s179.086-400 400-400 400 179.086 400 400Z"
             stroke="#D4D4D4"
-            strokeOpacity="1"
+            strokeOpacity="0.7"
           />
           <path
             d="M913 513c0 220.914-179.086 400-400 400"
@@ -88,7 +88,7 @@ export const Hero = () => {
               gradientUnits="userSpaceOnUse"
             >
               <stop stopColor="#43cea2" />
-              <stop offset="1" stopColor="#43cea2" stopOpacity="50" />
+              <stop offset="1" stopColor="#43cea2" stopOpacity="0" />
             </linearGradient>
           </defs>
         </svg>
