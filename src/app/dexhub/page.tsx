4d1b2c4b11861c62943912ac3f5fdf16f0dbd6cb
unknown <zaynzafar18@gmail.com>
2023-12-19 20:39:45 +0500

    make a few adjustments

--- src/app/dexhub/page.tsx
+++ src/app/dexhub/page.tsx
@@ -47,7 +47,7 @@ const page = () => {
           </div>
         </div>
         <h2 className="text-center py-4 mt-16">More Snaps</h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-1 items-center gap-y-10 sm:max-w-xl sm:grid-cols-3 lg:mx-0 lg:max-w-none cursor-pointer">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-1 items-center gap-y-10 sm:max-w-xl lg:grid-cols-3 lg:mx-0 lg:max-w-none cursor-pointer">
           <div className="hover:scale-105 ease-in duration-100 flex flex-col items-center">
             <Image
               className="col-span-1 max-h- w-full object-cover sm:col-start-2 rounded-xl"
