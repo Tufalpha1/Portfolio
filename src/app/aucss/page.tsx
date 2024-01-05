29e27ee18fea40db0e58b9bed0a025daaf04722a
unknown <zaynzafar18@gmail.com>
2024-01-05 20:51:41 +0500

    update site

--- src/app/aucss/page.tsx
+++ src/app/aucss/page.tsx
@@ -17,7 +17,7 @@ const page = () => {
           className="absolute z-1"
           layout="fill"
           objectFit="cover"
-          // src={aucssImg}
+          src={aucssImg}
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
