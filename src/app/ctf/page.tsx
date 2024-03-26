7744dc11e4272d155dcc80d5cdc5852079cdf120
Tufalpha1 <zaynzafar18@gmail.com>
2024-03-26 17:36:18 +0500

    update some stuff

--- src/app/ctf/page.tsx
+++ src/app/ctf/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import ctfImg from "../../../public/assets/projects/ctf-scoreboard.png";
 import firstBlood from "../../../public/assets/projects/first-blood.png";
 import newLeader from "../../../public/assets/projects/new-leader.png";
-import ctf from "../../../public/assets/projects/ctf-scoreboard.png";
+import ctf from "../../../public/assets/projects/ctf.png";
 import winner from "../../../public/assets/projects/winner.png";
 import winner2 from "../../../public/assets/projects/winner-2.png";
 import winner3 from "../../../public/assets/projects/winner-3.png";
@@ -18,7 +18,7 @@ const page = () => {
           className="absolute z-1"
           layout="fill"
           objectFit="cover"
-          src={ctf}
+          src={ctfImg}
           alt="/"
         />
         <div className="tracking-tighter absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
@@ -65,7 +65,7 @@ const page = () => {
           <div className="hover:scale-105 ease-in duration-100 flex flex-col items-center rounded-xl shadow-xl shadow-gray-700">
             <Image
               className="col-span-1 col-start-2 max-h-full w-full object-cover sm:col-start-auto rounded-xl"
-              src={ctfImg}
+              src={ctf}
               alt="Leaderboard-and-solves"
               width={700}
               height={700}
--- src/app/siliconvillage/page.tsx
+++ src/app/siliconvillage/page.tsx
@@ -39,7 +39,7 @@ const page = () => {
             first and largest tech hub in Pakistan. The project is being built by ByteBoltSec's team.`}
           </p>
           <div className="mt-10 flex justify-center">
-            <button className="text-black font-bold bg-gradient-to-r from-[#40e0d0] via-[#55dada] to-[#1d3463] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            <button className="text-slate-50 font-bold bg-gradient-to-r from-[#40e0d0] via-[#55dada] to-[#1d3463] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
               <Link href="https://silicon-village.vercel.app">Demo</Link>
             </button>
             <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
